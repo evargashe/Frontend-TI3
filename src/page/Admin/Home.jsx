b4d839a9fa2ffb525558f33a6c9b9deb0b209e17
Edwar Jhoel Vargas <edwarjhoel.99@gmail.com>
2024-01-29 20:38:04 -0500

    cambios

--- src/page/Admin/Home.jsx
+++ src/page/Admin/Home.jsx
@@ -20,9 +20,12 @@ function HomeAdmin(props) {
                     <a className="btn">
                         <Link to="/admin/equipos-solicitados">
                             <p>Ver Equipos Solicitados</p>
-                            <img src={Calendar} alt="Ver Equipos Solicitados" className={styles.styleImg}/>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6">
+                                <path d="M12.75 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM7.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM8.25 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM9.75 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM10.5 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM12.75 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM14.25 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 17.25a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 15.75a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5ZM15 12.75a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM16.5 13.5a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Z" />
+                                <path fillRule="evenodd" d="M6.75 2.25A.75.75 0 0 1 7.5 3v1.5h9V3A.75.75 0 0 1 18 3v1.5h.75a3 3 0 0 1 3 3v11.25a3 3 0 0 1-3 3H5.25a3 3 0 0 1-3-3V7.5a3 3 0 0 1 3-3H6V3a.75.75 0 0 1 .75-.75Zm13.5 9a1.5 1.5 0 0 0-1.5-1.5H5.25a1.5 1.5 0 0 0-1.5 1.5v7.5a1.5 1.5 0 0 0 1.5 1.5h13.5a1.5 1.5 0 0 0 1.5-1.5v-7.5Z" clipRule="evenodd" />
+                            </svg>
                         </Link>
-                        
+
                     </a>
                 </div>
                 <div className={styles.stylesInfo}>
@@ -31,7 +34,7 @@ function HomeAdmin(props) {
                             <p>Devolucion Equipos</p>
                             <img src={OverTime} alt="Devolucion Equipos" className={styles.styleImg} />
                         </Link>
-                        
+
                     </a>
                 </div>
                 <div className={styles.stylesInfo}>
@@ -40,7 +43,7 @@ function HomeAdmin(props) {
                             <p>Ver Equipos</p>
                             <img src={OverTime} alt="Agregar Equipos" className={styles.styleImg} />
                         </Link>
-                        
+
                     </a>
                 </div>
                 <div className={styles.stylesInfo}>
@@ -49,7 +52,7 @@ function HomeAdmin(props) {
                             <p>Agregar Equipos</p>
                             <img src={OverTime} alt="Agregar Equipos" className={styles.styleImg} />
                         </Link>
-                        
+
                     </a>
                 </div>
                 <div className={styles.stylesInfo}>
@@ -58,7 +61,7 @@ function HomeAdmin(props) {
                             <p>Reportes</p>
                             <img src={MultipleSmarphones} alt="Reportes" className={styles.styleImg} />
                         </Link>
-                        
+
                     </a>
                 </div>
             </div>
